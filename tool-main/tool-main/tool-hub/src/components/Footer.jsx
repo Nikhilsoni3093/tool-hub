0a6c07c6d9e29f424e5f2c839dc99d4031472969
Nikhil soni <164142208+Nikhilsoni3093@users.noreply.github.com>
2026-06-13 00:32:39 +0530

    Update Footer.jsx

--- tool-main/tool-main/tool-hub/src/components/Footer.jsx
+++ tool-main/tool-main/tool-hub/src/components/Footer.jsx
@@ -6,7 +6,7 @@ export default function Footer() {
     return (
       <footer className="mt-16 bg-gray-100 py-6 text-center text-sm text-gray-600">
         Made with ❤️ by coders786 · Connect:
-        <a href="https://github.com/iamvis" className="text-blue-600 ml-1" target="_blank" rel="noopener noreferrer">GitHub</a>
+        <a href="https://github.com/Nikhilsoni3093" className="text-blue-600 ml-1" target="_blank" rel="noopener noreferrer">GitHub</a>
       </footer>
     );
   }
